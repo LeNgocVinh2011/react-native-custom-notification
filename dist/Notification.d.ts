0c0c13d56c0c9c4be7d282eae122ce97ec3115af
lengocvinh271299 <lengocvinh27121999@gmail.com>
2024-01-23 17:34:16 +0700

    Rebuild module

--- dist/Notification.d.ts
+++ dist/Notification.d.ts
@@ -1,5 +1,5 @@
-/// <reference types="react" />
+import React from 'react';
 import { INotificationContext, INotiticationProps } from './types';
 export declare function useNotification(): INotificationContext;
-declare function Notification({ children }: INotiticationProps): JSX.Element;
+declare function Notification({ children }: INotiticationProps): React.JSX.Element;
 export default Notification;
